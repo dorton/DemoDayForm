7438c6e7c536f4278f86c9028e8d9adf9fc612bb
Brian Dorton <dorton@gmail.com>
2015-07-22 14:07:25 -0500

    removes bio

--- app/models/student.js
+++ app/models/student.js
@@ -8,7 +8,7 @@ export default DS.Model.extend({
   portfolio_url: DS.attr('string'),
   profile_pic_link: DS.attr('string'),
   course: DS.attr('string'),
-  bio: DS.attr('string'),
+  // bio: DS.attr('string'),
   project: DS.belongsTo('project', { async: true }),
 
 
